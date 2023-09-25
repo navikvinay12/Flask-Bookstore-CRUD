bb2e006b275614e6347e55c4a329319cd14964c6
navikvinay12 <Navik.vinay12@gmail.com>
2023-09-25 21:07:08 +0530

    [Vinay] Add: decorator fns verify_user superuser to be accessible by APIs

--- core/utils.py
+++ core/utils.py
@@ -12,6 +12,7 @@ def verify_user(func):
         base_url = ":".join(request.url_root.split(":")[:-1])
         response = httpx.get(f"{base_url}:{setting.USER_PORT}/retrieve_user", params={"token": token})
         if response.status_code == 200:
+            print(response.json())
             kwargs.update(current_user=response.json())
         elif response.status_code >= 400:
             return make_response({"msg": "User not found"}, 401)
