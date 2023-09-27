5be2027f3f7b18afcdbf01b7d3180af389dae89e
navikvinay12 <Navik.vinay12@gmail.com>
2023-09-27 17:17:28 +0530

    [Vinay] Add: Added cart and order APIs

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from user.routes import app as user
 from book.routes import app as book
+from cart.routes import app as cart
 
 if __name__ == '__main__':
     user.run(debug=True)
     book.run(debug=True)
+    cart.run(debug=True)
